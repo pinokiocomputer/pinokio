652425513705f37ab84653013b75d46498524a81
cocktailpeanut <cocktailpeanuts@proton.me>
2025-09-09 12:14:32 -0400

    background

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const config = require('./config')
 const pinokiod = new Pinokiod(config)
 let mode = pinokiod.kernel.store.get("mode") || "full"
 //iprocess.env.PINOKIO_MODE = process.env.PINOKIO_MODE || 'desktop';
-if (mode === 'minimal') {
+if (mode === 'minimal' || mode === 'background') {
   require('./minimal');
 } else {
   require('./full');
