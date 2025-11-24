f2955ba270b78da2b795592280c1351a64918fdd
cocktailpeanut <cocktailpeanuts@proton.me>
2025-11-23 22:50:38 -0500

    3.330
    A/B testing to check if the session is causing networking issues in some gradio apps (fluxgym)

--- full.js
+++ full.js
@@ -1912,7 +1912,7 @@ const createWindow = (port) => {
     height: mainWindowState.height,
     minWidth: 190,
     webPreferences: {
-      session: session.fromPartition('temp-window-' + Date.now()),
+      session: session.defaultSession,
       webSecurity: false,
       nativeWindowOpen: true,
       contextIsolation: false,
@@ -1977,7 +1977,7 @@ const loadNewWindow = (url, port) => {
     height: winState.height,
     minWidth: 190,
     webPreferences: {
-      session: session.fromPartition('temp-window-' + Date.now()),
+      session: session.defaultSession,
       webSecurity: false,
       nativeWindowOpen: true,
       contextIsolation: false,
